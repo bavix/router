3aebde61be4395b0a564025da2496f0924eae2fa
rez1dent3 <maksim.babichev95@gmail.com>
2017-01-18 11:28:35 +0300

    patch test

--- demo/test.php
+++ demo/test.php
@@ -2,18 +2,31 @@
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
-$route = new \Deimos\Route\Route(
-    ['(/<lang:[a-z]{2}>)/<controller>(/<action>(/<id:\d+>))'],
-    [
-        'lang'   => 'ru',
-        'action' => 'default'
-    ]
-);
-
 $router = new \Deimos\Router\Router();
 
 $router->setMethod('GET');
-$router->addRoute($route);
+
+
+//$route = new \Deimos\Route\Route(
+//    ['(/<lang:[a-z]{2}>)/<controller>(/<action>(/<id:\d+>))'],
+//    [
+//        'lang'   => 'ru',
+//        'action' => 'default'
+//    ]
+//);
+//$router->addRoute($route); // OR
+
+$router->setRoutes([
+    [
+        'type'     => 'pattern',
+        'path'     => '(/<lang:[a-z]{2}>)/<controller>(/<action>(/<id:\d+>))',
+
+        'defaults' => [
+            'lang'   => 'ru',
+            'action' => 'default'
+        ]
+    ]
+]);
 
 var_dump($route = $router->getCurrentRoute('/hello-world'), $route->attributes());
 var_dump($route = $router->getCurrentRoute('/en/hello-world'), $route->attributes());
