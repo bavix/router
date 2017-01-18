95df82cadbf5e33c56fa2ac11ff66001829b7f35
rez1dent3 <maksim.babichev95@gmail.com>
2017-01-18 11:33:03 +0300

    add docs

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Deimos\\Router\\Route"],["c","Deimos\\Router\\Router"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Throwable"]];
+ApiGen.elements = [["c","Deimos\\Router\\Exceptions\\PathNotFound"],["c","Deimos\\Router\\Exceptions\\TypeNotFound"],["c","Deimos\\Router\\Prefix"],["c","Deimos\\Router\\Route"],["c","Deimos\\Router\\Router"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Throwable"]];
